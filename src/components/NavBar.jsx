bd04cf855fca4e14ca8760e9e5e283b5fe61c967
Raijin1302 <raijinnguyen1302@gmail.com>
2023-02-20 23:57:36 +0700

    adding css & done Navbar  :green_circle:

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const [toggle, setToggle] = useState(false);
+
+  const handleTrigger = () => setToggle(!toggle);
+  return (
+    <nav className=" w-full flex justify-between items-center py-6 navbar">
+      <img className="w-[124px] h-[32px]" src={logo} alt="" />
+      <ul className="list-none hidden sm:flex flex-1 justify-end ">
+        {navLinks.map((nav, index) => (
+          <li
+            className={`font-poppins text-base font-normal cursor-pointer text-white ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } `}
+            key={nav.id}
+          >
+            <a href={nav.id}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      {/* For mobile */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu-mobile"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={handleTrigger}
+        />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-1 flex-col justify-end ">
+            {navLinks.map((nav, index) => (
+              <li
+                className={`font-poppins text-base font-normal cursor-pointer text-white ${
+                  index === navLinks.length - 1 ? "mb-0" : "mb-4"
+                } `}
+                key={nav.id}
+              >
+                <a href={nav.id}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+    },
   },
   plugins: [],
 };
