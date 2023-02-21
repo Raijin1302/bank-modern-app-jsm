a2a2fd0d0f909a12852275da3327447b1682a014
Raijin1302 <raijinnguyen1302@gmail.com>
2023-02-21 23:27:17 +0700

    done Stats component :green_circle: :waning_crescent_moon:

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import { stats } from "../constants";
+import styles from "../style";
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <section
+      className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}
+    >
+      {stats.map((stat) => (
+        <div
+          key={stat.id}
+          className="flex flex-row justify-start items-center flex-1 m-3"
+        >
+          <h4 className="text-white font-poppins font-semibold xs:text-4xl text-3xl xs:leading-10 leading-9">
+            {stat.value}
+          </h4>
+          <p className="text-gradient font-poppins font-normal xs:text-xl text-lg xs:leading-7 leading-5 uppercase ml-3">
+            {stat.title}
+          </p>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default Stats;
