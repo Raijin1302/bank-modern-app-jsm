4fa835ceaddc471dba2190cd356c53b8c73b7a88
Raijin1302 <raijinnguyen1302@gmail.com>
2023-02-23 11:26:33 +0700

    done Business,Button  component :green_circle: :sunny:

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,6 +2,26 @@ import React from "react";
 import { features } from "../constants";
 import styles, { layout } from "../style";
 import Button from "./Button";
+
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div
+    className={`flex flex-row p-6 rounded-2xl feature-card ${
+      index === features.length - 1 ? "mb-0" : "mb-6"
+    }`}
+  >
+    <div className="w-[64px] h-[64px] rounded-full flex justify-center items-center bg-dimBlue">
+      <img src={icon} alt="icon" className="w-1/2 h-1/2 object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-lg mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-semibold text-dimWhite text-base mb-1">
+        {content}
+      </p>
+    </div>
+  </div>
+);
 const Business = () => {
   return (
     <section id="features" className={layout.section}>
@@ -15,7 +35,12 @@ const Business = () => {
           building credit, earning rewards and saving money. But with hundreds
           of credit cards on the market.
         </p>
-        <Button />
+        <Button styles="mt-10" />
+      </div>
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
       </div>
     </section>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-lg text-primary outline-none rounded-lg ${styles}`}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
