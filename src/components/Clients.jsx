472ec3a1d9890070f508fc28d35584c953f9ecb5
Raijin1302 <raijinnguyen1302@gmail.com>
2023-02-24 23:59:47 +0700

    done CLient,FeedbackCard,Testimonials :green_circle:

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { clients } from "../constants";
+import styles from "../style";
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <section className="flex justify-center items-center my-6">
+      <div className="flex justify-center items-center flex-wrap w-full ">
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className="flex justify-center items-center flex-1 sm:min-w-[192px] min-w-[120px]"
+          >
+            <img
+              className="sm:min-w-[192px] w-[120px] object-contain"
+              src={client.logo}
+              alt={client.logo}
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
